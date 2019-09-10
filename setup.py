f823314afda976671855c5696cd49b7dbe2a22c8
Thomas <thomaskok93@gmail.com>
2019-09-10 13:05:27 +0200

    Initial project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+  readme = f.read()
+
+setup(
+  name='multimedia-retrieval',
+  description='content-based retrieval system for 3d shapes',
+  long_description=readme,
+  author='Ruben Schenkhuizen, Thomas Kok',
+  author_email='r.schenkhuizen@students.uu.nl, t.t.kok@students.uu.nl',
+  packages=find_packages()
+)
