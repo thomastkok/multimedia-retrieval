a621fff6c6042e95c2f58dd1a1c137f24cd15679
Thomas <thomaskok93@gmail.com>
2019-09-12 14:33:46 +0200

    fix: cleanup

--- multimedia_retrieval/app.py
+++ multimedia_retrieval/app.py
@@ -21,17 +21,3 @@ def run():
     if off:
         os.remove('./temp.ply')
     open3d.visualization.draw_geometries([mesh])
-
-
-# from https://stackoverflow.com/questions/31129968/off-files-on-python
-def read_off(file_path):
-    with open(file_path, 'r') as off_file:
-        if 'OFF' != off_file.readline().strip():
-            raise('Not a valid OFF header')
-        n_verts, n_faces, n_edges = tuple(
-            [int(s) for s in off_file.readline().strip().split(' ')])
-        verts = [[float(s) for s in off_file.readline().strip().split(' ')]
-                 for i_vert in range(n_verts)]
-        faces = [[int(s) for s in off_file.readline().strip().split(' ')][1:]
-                 for i_face in range(n_faces)]
-        return verts, faces
