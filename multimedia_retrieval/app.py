c399aeee9c7c2865af832cf30bc0dcffcf7469b8
Thomas <thomaskok93@gmail.com>
2019-09-13 12:30:15 +0200

    feat: added a function to read a preset dataset

--- multimedia_retrieval/app.py
+++ multimedia_retrieval/app.py
@@ -4,6 +4,10 @@ import multimedia_retrieval.datasets.datasets as datasets
 
 
 def run():
-    file_path = input('Please input the file path of the .OFF or .PLY file:')
+    file_path = input('Please input the file path of the dataset:  ')
+    dataset = input('Please specify the dataset (princeton/labeled): ')
+    meshes = datasets.read_dataset(dataset, file_path, 10)
+    print(meshes)
+    file_path = input('Please input the file path of the .OFF or .PLY file: ')
     mesh = datasets.read_mesh(file_path)
     open3d.visualization.draw_geometries([mesh])
--- multimedia_retrieval/datasets/datasets.py
+++ multimedia_retrieval/datasets/datasets.py
@@ -5,19 +5,47 @@ import trimesh
 
 
 def read_mesh(file_path):
+    """
+    Reads the mesh file located at the specified file path,
+    and returns the mesh as open3d object.
+    """
     off = False
     if file_path.endswith('.off'):
         mesh = trimesh.load_mesh(file_path)
         trimesh.exchange.export.export_mesh(mesh, './temp.ply', 'ply')
-        print('Saved .OFF file as .PLY file')
         file_path = './temp.ply'
         off = True
     if file_path.endswith('.ply'):
         mesh = open3d.io.read_triangle_mesh(file_path)
-        print(type(mesh))
-        print('Read .PLY file')
     else:
         raise ValueError('Input file must be either .OFF or .PLY format')
     if off:
         os.remove('./temp.ply')
     return mesh
+
+
+def read_dataset(dataset, file_path, n_meshes=None):
+    """
+    Reads either the princeton or labeled dataset,
+    located at the specified file path,
+    and returns a dictionary of n meshes.
+    """
+    meshes = {}
+    n_meshes_loaded = 0
+    if dataset == 'princeton':
+        file_path = file_path + '\\db'
+    elif dataset != 'labeled':
+        raise ValueError(f'{dataset} is not a known dataset, \
+                         should be either "princeton" or "labeled"')
+    for root, dirs, files in os.walk(file_path, topdown=True):
+        if files:
+            for file in files:
+                if file.endswith('.off'):
+                    mesh = read_mesh(root + '\\' + file)
+                    index = file.split('.', 1)[0].replace('m', '')
+                    meshes[index] = mesh
+                    if n_meshes:
+                        n_meshes_loaded = n_meshes_loaded + 1
+                        if n_meshes_loaded >= n_meshes:
+                            return meshes
+    return meshes
