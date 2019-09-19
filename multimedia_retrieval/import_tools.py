e4ddb71edfe7547ba5a7538172e431c104d0eae1
Thomas <thomaskok93@gmail.com>
2019-09-19 15:24:49 +0200

    refactor: clean up the code for the mesh filter

--- multimedia_retrieval/import_tools.py
+++ multimedia_retrieval/import_tools.py
@@ -2,3 +2,4 @@ import sys
 
 
 sys.path.append(sys.path[0] + '/../datasets')
+sys.path.append(sys.path[0] + '/../processing')
--- multimedia_retrieval/processing/helpers.py
+++ multimedia_retrieval/processing/helpers.py
@@ -2,15 +2,15 @@ import os
 import sys
 import numpy as np
 
-import import_tools
-from datasets import read_mesh
+import multimedia_retrieval.import_tools
+from multimedia_retrieval.datasets.datasets import read_mesh
 
 
-def get_class_name(file_name, dataset_name):
+def get_class_name(file, dataset):
     # Dictionary that says which file belongs to which class for the dataset.
-    files_classes = {}
-    if dataset_name == 'princeton':
-        with open(file_name) as f:
+    classes = {}
+    if dataset == 'princeton':
+        with open(file) as f:
             lines = f.readlines()
             last_class_name = ''
             for line in lines:
@@ -19,38 +19,22 @@ def get_class_name(file_name, dataset_name):
                 if len(split_line) > 2 and split_line[1] == '0':
                     last_class_name = split_line[0]
                 elif len(split_line) == 1 and split_line[0].isdigit():
-                    files_classes[split_line[0]] = last_class_name
-        return files_classes
+                    classes[split_line[0]] = last_class_name
+        return classes
+    else:
+        raise ValueError(f'Dataset {dataset} not implemented yet')
 
 
-def get_mesh_properties(files_classes):
+def get_mesh_properties(meshes, classes):
     mesh_props = {}
-    for root, dirs, files in os.walk('../benchmark', topdown=True):
-        # print('root', root)
-        # print('dirs', dirs)
-        # print('files', files)
-        if files:
-            for elem in files:
-                if elem.endswith('.off'):
-                    # read mesh
-                    # get some properties from the mesh object
-                    # create dict entry for each mesh with stats/properties
-                    # add class to same dict entry
-                    mesh = read_mesh(root + '/' + elem)
-                    stripped_name = elem.split('.', 1)[0].replace('m', '')
-                    class_label = files_classes[stripped_name]
-                    properties = {}
-                    properties['class'] = class_label
-                    properties['nr_faces'] = len(mesh.triangles)
-                    properties['nr_vertices'] = len(mesh.vertices)
-                    properties['faces_type'] = 'triangles' # by definition
-                    properties['bounding_box'] = mesh.get_axis_aligned_bounding_box()
-                    mesh_props[mesh] = properties
-                    print(f'{mesh} has properties: {properties}')
+    for mesh_name in meshes.keys():
+        properties = {}
+        class_label = classes[mesh_name]
+        mesh = meshes[mesh_name]
+        properties['class'] = class_label
+        properties['nr_faces'] = len(mesh.triangles)
+        properties['nr_vertices'] = len(mesh.vertices)
+        properties['faces_type'] = 'triangles'  # by definition
+        properties['bounding_box'] = mesh.get_axis_aligned_bounding_box()
+        mesh_props[mesh] = properties
     return mesh_props
-
-
-file_path = '../benchmark/classification/v1/base/'
-files_classes = {**get_class_name(file_path + 'test.cla', 'princeton'),
-                 **get_class_name(file_path + 'train.cla', 'princeton')}
-get_mesh_properties(files_classes)
