c9d8626a5a51f0f6d426ad26be9e595554fa27cb
Thomas <thomaskok93@gmail.com>
2019-10-08 13:51:12 +0200

    matching structure

---
+++ multimedia_retrieval/matching/matching.py
@@ -0,0 +1,7 @@
+def match_shapes(mesh, dataset, k=None):
+    # Calculate features for mesh
+    # Get features for all meshes in dataset (pre-calculated)
+    # Compute distance between query mesh and all meshes in dataset
+    # Sort distances from low to high
+    # Return the k best matching shapes
+    pass
