5707c2298482e4d3ebd3780f2ecc111d8ef318b2
Thomas <thomaskok93@gmail.com>
2019-10-08 14:02:38 +0200

    fix: matching shapes

--- multimedia_retrieval/matching/matching.py
+++ multimedia_retrieval/matching/matching.py
@@ -1,7 +1,18 @@
-def match_shapes(mesh, dataset, k=None):
-    # Calculate features for mesh
+from .distances import euclidean
+
+
+def match_shapes(mesh, dataset, mesh_feat, ds_feat, k=None):
+    # Get features for mesh
     # Get features for all meshes in dataset (pre-calculated)
+    # For now, both are parameters
     # Compute distance between query mesh and all meshes in dataset
+    results = {}
+    for data_point in ds_feat:
+        dist = euclidean(mesh_feat, ds_feat[data_point])
+        results[data_point] = dist
     # Sort distances from low to high
     # Return the k best matching shapes
-    pass
+    if not k:
+        return sorted(results, key=results.get)
+    else:
+        return sorted(results, key=results.get)[:k]
