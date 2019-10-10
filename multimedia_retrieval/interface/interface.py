a427ebc5bbbb82187df81b866854886a211576f6
Thomas <thomaskok93@gmail.com>
2019-10-10 14:10:57 +0200

    feat: visualize the best matching shapes, one by one

--- multimedia_retrieval/interface/interface.py
+++ multimedia_retrieval/interface/interface.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as gui
 from multimedia_retrieval.matching.matching import query_shape
+from multimedia_retrieval.visualization.visualization import draw_mesh
 
 
 def create_interface():
@@ -21,6 +22,8 @@ def create_interface():
             dataset = values[0]
             mesh = values[1]
             shapes = query_shape(mesh, dataset)
+            for shape in shapes:
+                draw_mesh(shape)
         print('You entered', values)
 
     window.close()
